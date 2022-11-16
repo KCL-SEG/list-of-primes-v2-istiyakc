6eced2a306693fa12c7f9e6a333e59d016e67894
istiyakc <istiyak.choudhury@kcl.ac.uk>
2022-11-16 01:37:19 +0000

    fnish

--- primes.py
+++ primes.py
@@ -3,4 +3,24 @@
 
 def primes(number_of_primes):
     list = []
+    number = 2
+    isPrime = True
+    doneSearch = False
+    if (number_of_primes <= 0):
+        raise ValueError("invalid number")
+    else:
+        while (len(list) < number_of_primes):
+            while(isPrime == True and doneSearch == False):
+                for i in range(2, number):
+                    if (i is not number):
+                        if (number%i == 0):
+                            isPrime = False
+                doneSearch = True
+            if(isPrime == True):
+                list.append(number)
+
+            number= number + 1
+            isPrime = True
+            doneSearch = False
+
     return list
